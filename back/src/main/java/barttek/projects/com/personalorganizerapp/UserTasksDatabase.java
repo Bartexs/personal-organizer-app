56266d6730104a594b9666a8afbb4bd1c2ad5ec7
Bartexs <barttosz.witkowski@gmail.com>
2022-11-06 16:17:28 +0100

    Added tests for taskToBeDone localDate property and taskDateCompleted property

---
+++ back/src/main/java/barttek/projects/com/personalorganizerapp/UserTasksDatabase.java
@@ -0,0 +1,44 @@
+package barttek.projects.com.personalorganizerapp;
+
+import org.apache.catalina.User;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+public class UserTasksDatabase {
+    private final List<UserTask> userTaskList = new ArrayList<>();
+
+    public UserTasksDatabase() {
+
+    }
+
+    public void addToUserTaskList(UserTask uTask) {
+        this.userTaskList.add(uTask);
+    }
+
+    public List<UserTask> getWholeUserTaskList() {
+        return this.userTaskList;
+    }
+
+    public Optional<UserTask> getUserTaskById(int userTaskId) {
+        return userTaskList.stream().filter(uTask -> uTask
+                        .getId() == userTaskId)
+                        .findFirst();
+    }
+
+    public void removeUserTaskById(int userTaskId) {
+        Optional<UserTask> userTaskOptional = getUserTaskById(userTaskId);
+        userTaskOptional.ifPresent(uTask -> uTask = null);
+        System.gc();
+    }
+
+    public boolean isUserTaskInUserTaskList(int userTaskId) {
+        return userTaskList.stream().anyMatch(t -> t.getId() == userTaskId);
+    }
+
+    public Stream<UserTask> getCompletedUserTasks() {
+        return userTaskList.stream().filter(UserTask::isCompleted);
+    }
+}
