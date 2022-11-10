7d0cc9b8ce5ac411a011c8d3c1cf99f727d43862
Bartexs <barttosz.witkowski@gmail.com>
2022-11-10 22:50:27 +0100

    Rewritten service

--- front/src/app/services/user-task.service.ts
+++ front/src/app/services/user-task.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { HttpParams } from "@angular/common/http";
-import { UserTask } from '../models/userTask.model';
+import { UserTask } from '../models/UserTask.model';
 
 @Injectable({
   providedIn: 'root'
@@ -20,16 +20,16 @@ export class UserTaskService {
 
   // fetch completed tasks via date
   onFetchCompletedTasksByDate(d: string) {
-
     // create new http parameters
     const parameters = new HttpParams().set("date", d);
-    let pathToGet = 'http://localhost:8080/date/user-task/'
+    let pathToGet = 'http://localhost:8080/user-task/completed/'
     return this.httpClient.get<UserTask[]>(pathToGet, {params: parameters});
   }
 
   // fetch NOT complted tasks via date
-  onFetchNotCompletedTasksByDate(dateOfTasksToShow: string) {
-    let pathToGet = 'http://localhost:8080/date/user-task/     '
-    return this.httpClient.get<UserTask[]>(pathToGet);
+  onFetchNotCompletedTasksByDate(d: string) {
+    const parameters = new HttpParams().set("date", d);
+    let pathToGet = 'http://localhost:8080/user-task/all/'
+    return this.httpClient.get<UserTask[]>(pathToGet, {params: parameters});
   }
 }
