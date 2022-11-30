1c5a3221407e8801978e73b33fc4d1a697861598
Bartexs <barttosz.witkowski@gmail.com>
2022-11-30 19:54:53 +0100

    Fixed showing message if both lists are empty

--- front/src/app/components/user-task-feature/user-task-controller/user-task-controller.component.ts
+++ front/src/app/components/user-task-feature/user-task-controller/user-task-controller.component.ts
@@ -1,4 +1,6 @@
+import { ThisReceiver } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
+import { UserTaskService } from 'src/app/services/user-task.service';
 import { UserTasksListService } from 'src/app/services/user-tasks-list.service';
 
 @Component({
@@ -10,19 +12,38 @@ import { UserTasksListService } from 'src/app/services/user-tasks-list.service';
 // component takes care of:
 // SHOWING WIDGET / FULL VIEW
 // SHOWING HEADER WITH BUTTONS & SHOWING CREATION ROW
-// SHOWING TASKS LISTS / ALL SCHEDULED TASKS
+// SHOWING TASKS LISTS / ALL SCHEDULED TASKS / BOTH LISTS EMPTY MESSAGE
 
 export class UserTaskControllerComponent implements OnInit {
   isWidgetView = true;
   showAllScheduledTasksList = false;
+  isBothListsEmpty = false;
 
-  constructor(private userTasksListService: UserTasksListService ) { 
+  constructor(private userTaskService: UserTaskService, private userTasksListService: UserTasksListService ) { 
 
   }
 
   ngOnInit(): void {
+    this.subscribeToGetDate();
+    this.subscribeToGetShowAllScheduledUserTasks();
+  }
+
+  public subscribeToGetDate() {
+    this.userTasksListService.getMessage().subscribe((date) => {
+      this.setIsBothListsEmpty(date);
+    })
+  }
+  
+  public setIsBothListsEmpty(date: string) {
+    this.userTaskService.getIsBothListsEmpty(date).subscribe((value) => {
+      this.isBothListsEmpty = value;
+    })
+  }
+
+  public subscribeToGetShowAllScheduledUserTasks() {
     this.userTasksListService.getShowAllSchedulesUserTasksObservable().subscribe((value) => {
       this.showAllScheduledTasksList = value;
     })
   }
 }
+
