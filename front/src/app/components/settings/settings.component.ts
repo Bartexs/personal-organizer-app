b11b6d5a5dbfcfa8b4c0b5b0a5a7e8f104bec6a6
Bartek <barttosz.witkowski@gmail.com>
2022-12-25 23:38:48 +0100

    Added emiting color schema from service as observable

--- front/src/app/components/settings/settings.component.ts
+++ front/src/app/components/settings/settings.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ColorSchema } from 'src/app/models/ColorSchema.model';
+import { AppearanceService } from './appearance.service';
 
 @Component({
   selector: 'app-settings',
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SettingsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private appearanceService: AppearanceService) { }
 
   ngOnInit(): void {
   }
 
+  public onSetColorSchema(colorSchemaText: String) {
+    switch(colorSchemaText) {
+      case "DARK": {
+        this.appearanceService.onChangeColorSchema(ColorSchema.DARK);
+        break;
+      }
+      case "LIGHT": {
+        this.appearanceService.onChangeColorSchema(ColorSchema.LIGHT);
+        break;
+      }
+      default: {
+        console.log("Such color schema not specified!");
+        break;
+      }
+    }
+  }
 }
--- front/src/app/models/ColorSchema.model.ts
+++ front/src/app/models/ColorSchema.model.ts
@@ -1,6 +1,7 @@
 export class ColorSchema {
     static readonly DARK = new ColorSchema("DARK", "Some dark color value - main");
     static readonly LIGHT = new ColorSchema("LIGHT", "Some light color value - main");
+    static readonly DEFAULT = new ColorSchema("DEFAULT", "Purple color");
 
     private constructor(private readonly key: string, public readonly mainColor: string) {
         
