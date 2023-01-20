686da9a9f2ea3338c8ef5c5668a0d5c9e01f6af1
Bartexs <barttosz.witkowski@gmail.com>
2023-01-20 23:55:14 +0100

    Implemented statistics info into ui

--- front/src/app/components/statistics-page/statistics-main/statistics-main.component.ts
+++ front/src/app/components/statistics-page/statistics-main/statistics-main.component.ts
@@ -2,21 +2,44 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { StatisticsSummary } from './StatisticsSummary';
 
+export interface TimeForUI {
+  hours: number, 
+  minutes: number
+}
+
 @Component({
   selector: 'app-statistics-main',
   templateUrl: './statistics-main.component.html',
   styleUrls: ['./statistics-main.component.css']
 })
+
 export class StatisticsMainComponent implements OnInit {
+  statisticsSummary!: StatisticsSummary;
+  timeSpentOnOrganizer!: TimeForUI;
+  longestSession!: TimeForUI;
+  isLoading = true; 
 
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.getStatisticsSummary();
   }
 
-  public getStatisticsSummary() {
+  public convertMinutesTimersForUI(minutesToConvert: number): {hours: number, minutes: number} {
+    let hours = Math.round(minutesToConvert / 60);
+    let minutes = minutesToConvert % 60;
+
+    return {hours, minutes};
+  }
+
+  // move it all to the statistics service
+  public async getStatisticsSummary() {
+    this.isLoading = true;
     this.onGetStatisticsSummary().subscribe((rec) => {
-      console.log(rec);
+      this.statisticsSummary = rec;
+      this.longestSession = this.convertMinutesTimersForUI(this.statisticsSummary.longestSessionsDuration);
+      this.timeSpentOnOrganizer = this.convertMinutesTimersForUI(this.statisticsSummary.timeSpentOnOrganizer);
+      this.isLoading = false;
     })
   }
 
